d293f14780f0f1ad9e4566efb4ec8c0e72fd73ea
tech-shagun <shtyagi2020@gmail.com>
2024-01-15 23:10:24 +0530

    Fibonacci Code

--- fibonacci.java
+++ fibonacci.java
@@ -2,16 +2,28 @@ import java.util.Scanner;
 
 public class fibonacci {
 
-    public static int fib(int num){
-        
+    public static void fib(int num){
+        int first = 0;
+        int next = 1;
+        System.out.println(first);
+        System.out.println(next);
+        for(int i = 2; i <= num; i++){
+           
+            int temp = first + next;
+            System.out.println(temp);
+            first = next;
+            next = temp;
+
+        }
     }
 
     public static void main(String[] args){
 
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
+        s.close();
 
-        System.out.println(fib(n));
+        fib(n);
     }
     
 }
